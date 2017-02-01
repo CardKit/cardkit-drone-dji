ab9e8dfe3ae2c875037803466660e8434fec495a
Saad Ismail <saad@us.ibm.com>
2017-02-01 11:57:03 -0500

    adding changes to  support normalized attitude

---
+++ DroneTokensDJI/Extensions/DJIAttitudeExtension.swift
@@ -0,0 +1,24 @@
+//
+//  DJIAttitudeExtension.swift
+//  DroneTokensDJI
+//
+//  Created by ismails on 2/1/17.
+//  Copyright © 2017 IBM. All rights reserved.
+//
+
+import Foundation
+
+import DJISDK
+
+extension DJIAttitude {
+    func normalize() -> DJIAttitude {
+        // From DJI Documentation
+        // Attitude of the aircraft where the pitch, roll, and yaw values will be in the range of [-180, 180]. If the values of the pitch, roll, and yaw are 0, the aircraft will be hovering level with a True North heading.
+        
+        let normalizedYaw = (yaw + 360).truncatingRemainder(dividingBy: 360)
+        let normalizedPitch = (pitch + 360).truncatingRemainder(dividingBy: 360)
+        let normalizedRoll = (roll + 360).truncatingRemainder(dividingBy: 360)
+        
+        return DJIAttitude(pitch: normalizedPitch, roll: normalizedRoll, yaw: normalizedYaw)
+    }
+}
